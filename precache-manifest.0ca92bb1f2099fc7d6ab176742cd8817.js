3e070ee954270d620d63d3b7d716ab473c6ec057
deannakazawa <imanardiansyah89@gmail.com>
2019-11-15 15:59:33 +0700

    Updates

---
+++ precache-manifest.0ca92bb1f2099fc7d6ab176742cd8817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630af4440afc5ff66ecf91016c8cdc90",
+    "url": "/learn-react-pertamax/index.html"
+  },
+  {
+    "revision": "48b461f4de91ee319706",
+    "url": "/learn-react-pertamax/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5610c3f63d250afe239e",
+    "url": "/learn-react-pertamax/static/js/2.c9501b9b.chunk.js"
+  },
+  {
+    "revision": "48b461f4de91ee319706",
+    "url": "/learn-react-pertamax/static/js/main.6ffdc64e.chunk.js"
+  },
+  {
+    "revision": "df8f8f52ce324a85549d",
+    "url": "/learn-react-pertamax/static/js/runtime-main.7421d777.js"
+  },
+  {
+    "revision": "ca8fc76b37f4757a95608d269d9dc27b",
+    "url": "/learn-react-pertamax/static/media/Orochimaru2.ca8fc76b.jpg"
+  }
+]);
